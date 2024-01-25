50a61e92b1b869bd7c2c4f0f4768596c66e713e4
Tarun Vijwani <tarun.vijwani@automattic.com>
2024-01-26 01:14:26 +0400

    Remove unnecessary comment for init

--- woocommerce-readonly-addressfields.php
+++ woocommerce-readonly-addressfields.php
@@ -13,13 +13,6 @@
  * @package           woocommerce-readonly-addressfields
  */
 
-/**
- * Registers the block using the metadata loaded from the `block.json` file.
- * Behind the scenes, it registers also all assets so they can be enqueued
- * through the block editor in the corresponding context.
- *
- * @see https://developer.wordpress.org/reference/functions/register_block_type/
- */
 add_action ( 'woocommerce_blocks_loaded', 'woocommerce_readonly_addressfields_init');
 
 function woocommerce_readonly_addressfields_init()
