7c22df1e76fec78f050393e37b2d7759ab0ca19d
Tarun Vijwani <tarun.vijwani@automattic.com>
2024-02-07 01:19:10 +0400

    Use checkoutfilters to set address fields read-only

--- src/js/hide-edit-button-checkout.js
+++ src/js/hide-edit-button-checkout.js
@@ -1,32 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-	var observer = new MutationObserver(function (mutations) {
-		mutations.forEach(function (mutation) {
-			if (mutation.addedNodes.length) {
-				let editButton = document.querySelector(
-					".wc-block-components-address-card__edit"
-				);
-				let useSameAddressForBilling = document.querySelector(
-					".wc-block-checkout__use-address-for-billing"
-				);
-				let shippingTitle = document.querySelector(
-					".wc-block-checkout__shipping-fields .wc-block-components-title"
-				);
-
-				if (shippingTitle && shippingTitle.textContent.includes("Shipping")) {
-					shippingTitle.textContent = "Shipping And Billing Address";
-				}
-				if (editButton) {
-					editButton.style.display = "none";
-				}
-				if (useSameAddressForBilling) {
-					useSameAddressForBilling.style.display = "none";
-				}
-			}
-		});
-	});
-
-	observer.observe(document.body, {
-		childList: true,
-		subtree: true,
-	});
+  setAddressFieldsReadOnly();
 });
+
+const isBillingAddressReadOnly = () => {
+  return true;
+};
+
+const isShippingAddressReadOnly = () => {
+  return true;
+};
+
+const setAddressFieldsReadOnly = () => {
+  const { registerCheckoutFilters } = window.wc.blocksCheckout;
+  registerCheckoutFilters("woocommerce-readonly-addressfields", {
+    isBillingAddressReadOnly: isBillingAddressReadOnly,
+    isShippingAddressReadOnly: isShippingAddressReadOnly,
+  });
+};
