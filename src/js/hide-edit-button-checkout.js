c1b379bae237874bfd20416d65a212d45f4eb964
Tarun Vijwani <tarun.vijwani@automattic.com>
2024-02-09 19:42:49 +0400

    Revert "Register fixed billing and shipping addresses filters"
    This reverts commit abf91f91d04174ee71688c1682c07cf06c1adcc2.

--- src/js/hide-edit-button-checkout.js
+++ src/js/hide-edit-button-checkout.js
@@ -9,41 +9,11 @@ const isBillingAddressReadOnly = () => {
 const isShippingAddressReadOnly = () => {
   return true;
 };
-const setFixedShippingAddress = () => {
-  console.log("setFixedBillingAddress called");
-  return {
-    first_name: "John",
-    last_name: "Doe",
-    company: "WooCommerce",
-    country: "US",
-    address_1: "123 Main St",
-    address_2: "",
-    city: "San Francisco",
-    state: "CA",
-    postcode: "94103",
-  };
-};
 
-const setFixedBillingAddress = () => {
-  console.log("setFixedBillingAddress called");
-  return {
-    first_name: "Test",
-    last_name: "Billing",
-    company: "WooCommerce Billing Co.",
-    country: "US",
-    address_1: "123 Main St Billing",
-    address_2: "",
-    city: "San Francisco",
-    state: "CA",
-    postcode: "94103",
-  };
-};
 const setAddressFieldsReadOnly = () => {
   const { registerCheckoutFilters } = window.wc.blocksCheckout;
   registerCheckoutFilters("woocommerce-readonly-addressfields", {
     isBillingAddressReadOnly: isBillingAddressReadOnly,
     isShippingAddressReadOnly: isShippingAddressReadOnly,
-    setFixedBillingAddress: setFixedBillingAddress,
-    setFixedShippingAddress: setFixedShippingAddress,
   });
 };
